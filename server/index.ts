114be848aebeab65913f133c70160910317cf90b
takic41479 <45742182-takic41479@users.noreply.replit.com>
2025-07-30 10:22:51 +0000

    Restored to '188b3d9c59df2f3c799076fb5a4af68d376805b0'
    Replit-Restored-To: 188b3d9c59df2f3c799076fb5a4af68d376805b0

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,8 @@ import express, { type Request, Response, NextFunction } from "express";
 import session from "express-session";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import connectDB from "./db";
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -49,6 +51,9 @@ app.use((req, res, next) => {
 });
 
 (async () => {
+  // Connect to MongoDB
+  await connectDB();
+  
   const server = await registerRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
